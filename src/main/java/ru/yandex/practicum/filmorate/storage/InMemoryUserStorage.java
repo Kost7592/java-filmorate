3bfcc17b20f67cbeb57ed7ed3b282b71f7a4812e
MKD <k.mihailow2018@yandex.ru>
2024-08-19 16:48:37 +0400

    Начата работа в новой ветке. Изменена архитектура.

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Component
+@RestControllerAdvice
+public class InMemoryUserStorage implements UserStorage {
+
+}
