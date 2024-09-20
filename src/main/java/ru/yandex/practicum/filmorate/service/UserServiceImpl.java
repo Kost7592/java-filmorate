61d5fcd1ae777bc2b66c5a0b574203b7294c8481
Kost7592 <k.mihailow2018@yandex.ru>
2024-09-20 22:38:17 +0400

    Checkstyle(1)

--- src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
@@ -6,11 +6,8 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.repository.UserRepository;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * UserServiceImpl — это сервис, который реализует функциональность UserService.
